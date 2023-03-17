8dd0f3fc4d63cd0c1a234df1a62e91456963bed5
Jameswong <wcf0123456789@gmail.com>
2023-03-17 16:20:54 +0800

    Added data structure

--- main.c
+++ main.c
@@ -6,6 +6,36 @@
 void home_menu();
 void login_menu();
 
+/* Data structure */
+typedef struct {
+    char session_code[50];
+    char title[50];
+    char day[50];
+    char start_time[50];
+    char location[50];
+    char tutor_id[50];
+} sessions;
+
+typedef struct {
+    char session_code[50];
+    char user_id[50];
+    char name[50];
+    char role[50];
+} enrolled_sessions;
+
+typedef struct {
+    char user_id[50];
+    char name[50];
+    char password[50];
+    char email[50];
+    char role[50];
+} users;
+
+typedef struct {
+    char tutor_id[50];
+    char title[50];
+} tutor_profile;
+
 /* Main function */
 int main() {
     home_menu();
